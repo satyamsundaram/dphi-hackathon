c692a7f928e75b3886cd09d87cac7d0144fe631d
satyam-01-byte <satyamsundaram01@gmail.com>
2022-10-18 12:15:21 +0530

    removed filters and search for now (incomplete features)

--- client/src/components/Explore.jsx
+++ client/src/components/Explore.jsx
@@ -6,8 +6,8 @@ function Explore() {
   return (
     <div>
       <div className='bg-[#002A3B]'>
-        <h3 className='text-xl text-white font-bold text-center mb-10 pt-10'>Explore Challenges</h3>
-        <Filters />
+        <h3 className='text-2xl text-white font-bold text-center py-10'>Explore Challenges</h3>
+        {/* <Filters /> */}
       </div>
         <Challenges />
     </div>
